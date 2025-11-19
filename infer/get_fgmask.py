2f6b7d0e3e498e398479a308a040d7b9eb2a5d35
Shuzhou Yang <65540262+Ysz2022@users.noreply.github.com>
2025-11-19 15:44:20 +0800

    Update get_fgmask.py

--- infer/get_fgmask.py
+++ infer/get_fgmask.py
@@ -409,11 +409,9 @@ with torch.no_grad():
     outputs = grounding_model(**inputs)
 
 results = processor.post_process_grounded_object_detection(
-    outputs,
-    inputs.input_ids,
-    box_threshold=0.4,
-    text_threshold=0.3,
-    target_sizes=[image.size[::-1]]
+    outputs=outputs,
+    threshold=0.4,                     # 统一用 threshold
+    target_sizes=[image.size[::-1]],   # (H, W)，你这行是对的
 )
 
 input_boxes = results[0]["boxes"].cpu().numpy()
